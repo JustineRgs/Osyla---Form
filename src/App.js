1f6429c0d2a8a57fafba3a27f01658b61cc25a86
JustineRgs <108766192+JustineRgs@users.noreply.github.com>
2023-02-12 23:50:42 +0100

    component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,74 @@ import "./App.css";
 import GroupTypeOfStore from "./components/GroupTypeOfStore.jsx/GroupTypeOfStore";
 
 function App() {
+  const groups = [
+    {
+      name: "Type de store",
+      description: null,
+      radios: [
+        {
+          input: {
+            type: "radio",
+            name: "radio",
+            id: "store_banne",
+          },
+          label: {
+            htmlFor: "store_banne",
+            text: "Toile de store banne",
+          },
+          img: {
+            alt: "Illustration d'une toile de store banne",
+            src: "",
+          },
+        },
+        {
+          input: {
+            type: "radio",
+            name: "radio",
+            id: "store_bras_droits",
+          },
+          label: {
+            htmlFor: "store_bras_droits",
+            text: "Toile de store bras droits",
+          },
+          img: {
+            alt: "Illustration d'une toile de store bras droits",
+            src: "",
+          },
+        },
+        {
+          input: {
+            type: "radio",
+            name: "radio",
+            id: "store_double_pente",
+          },
+          label: {
+            htmlFor: "store_double_pente",
+            text: "Toile de store double pente",
+          },
+          img: {
+            alt: "Illustration d'une toile de store double pente",
+            src: "",
+          },
+        },
+        {
+          input: {
+            type: "radio",
+            name: "radio",
+            id: "store_desc_verticale",
+          },
+          label: {
+            htmlFor: "store_desc_verticale",
+            text: "Toile de store descente verticale",
+          },
+          img: {
+            alt: "Illustration d'une toile de store à descente verticale",
+            src: "",
+          },
+        },
+      ],
+    },
+  ];
   return (
     <div className="stage">
       <div className="block">
@@ -15,7 +83,9 @@ function App() {
           <p className="text_secondary_block">ou 3 mensualités de 00,00€</p>
         </div>
       </div>
-      <GroupTypeOfStore />
+      {groups.map((group, i) => (
+        <GroupTypeOfStore name={group.name} radios={group.radios} key={i} />
+      ))}
     </div>
   );
 }
